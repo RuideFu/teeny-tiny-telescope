fc672b4835b0ecd0745f5cdef52a011af0d5128f
Reed Fu <rfugithub@outlook.com>
2025-08-13 18:04:43 -0400

    update on-off time_stamp to be consitent for one bos

--- on_off.py
+++ on_off.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from enum import Enum
 import os
-from time import sleep
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,7 +8,7 @@ from matplotlib import pyplot as plt
 from ttt.rtlsdr import RTLSDR
 from ttt.plots import plot_spectrum
 
-INTEGRATION_TIME = 300  # seconds
+INTEGRATION_TIME = 10  # seconds
 GAIN = 50  # dB
 DATA_PATH = "data"  # Directory to save data files
 
@@ -36,7 +35,12 @@ class SpectrumType(Enum):
     PROCESSED = "processed"
 
 
-def file_name(spectrum_type: SpectrumType, gain: int, integration_time: float) -> str:
+def file_name(
+    spectrum_type: SpectrumType,
+    time_stamp: datetime,
+    gain: int,
+    integration_time: float,
+) -> str:
     """
     Generate a file name based on the spectrum type, gain, and integration time.
     Args:
@@ -47,7 +51,7 @@ def file_name(spectrum_type: SpectrumType, gain: int, integration_time: float) -
         str: Generated file name.
     """
 
-    return f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_{spectrum_type.value}_{gain}db_{integration_time}.npy"
+    return f"{time_stamp.strftime('%Y%m%d_%H%M%S')}_{spectrum_type.value}_{gain}db_{integration_time}.npy"
 
 
 def save_spectrum(freqs: np.ndarray, powers: np.ndarray, filename: str):
@@ -67,6 +71,7 @@ def save_spectrum(freqs: np.ndarray, powers: np.ndarray, filename: str):
 
 
 if __name__ == "__main__":
+    time_stamp = datetime.now()
     with RTLSDR(integration_time=INTEGRATION_TIME, gain=GAIN) as rtl:
 
         # off observation first:
@@ -75,7 +80,7 @@ if __name__ == "__main__":
         )
 
         freqs, powers, overhead_time = rtl.take_exposure()
-        off_filename = file_name(SpectrumType.OFF, GAIN, INTEGRATION_TIME)
+        off_filename = file_name(SpectrumType.OFF, time_stamp, GAIN, INTEGRATION_TIME)
         save_spectrum(freqs, powers, off_filename)
 
         # take on observation:
@@ -83,15 +88,13 @@ if __name__ == "__main__":
             ["Taking On Observation", "Point the antenna towards the source"]
         )
         freqs, powers, overhead_time = rtl.take_exposure()
-        on_filename = file_name(SpectrumType.ON, GAIN, INTEGRATION_TIME)
+        on_filename = file_name(SpectrumType.ON, time_stamp, GAIN, INTEGRATION_TIME)
         save_spectrum(freqs, powers, on_filename)
-
-
 
     # open off observation file
     off_data = np.load(os.path.join(DATA_PATH, off_filename))
     off_freqs, off_powers = off_data[:, 0], off_data[:, 1]
-    
+
     # open on observation file
     on_data = np.load(os.path.join(DATA_PATH, on_filename))
     on_freqs, on_powers = on_data[:, 0], on_data[:, 1]
